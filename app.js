95f59dd501bf5037323735eb3dfc5034d8e601b1
unknown <ali_jlidi85@yahoo.com>
2022-03-15 22:12:18 +0100

    Fix some frontend issues

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+
 //const _ = require("lodash");
 //use express to ease the work
 const app = express();
